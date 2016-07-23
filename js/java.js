98ae44a3fb2d493c828615acd28b2d45b19aaa15
Korirmitchelle <korirmitchelle@gmail.com>
2016-07-23 11:27:20 +0300

    replacing jQuery with $

--- js/java.js
+++ js/java.js
@@ -1,9 +1,11 @@
-jQuery("h1").click(function () {
-    alert("This is a header!");
-});
-jQuery("p").click(function () {
-    alert("This is a paragraph!");
-});
-jQuery("img").click(function () {
-    alert("This is an image!");
+$(document).ready(function () {
+    $("h1").click(function () {
+        alert("This is a header!");
+    });
+    $("p").click(function () {
+        alert("This is a paragraph!");
+    });
+    $("img").click(function () {
+        alert("This is an image!");
+    });
 });
